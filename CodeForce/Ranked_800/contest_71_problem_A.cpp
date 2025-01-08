0814a1a4902e59b07575ad65a42062fa2946b302
Sherif213 <Shouldtheone@gmail.com>
2025-01-08 03:12:15 +0300

    Solving Problem A from Contest 71 A

---
+++ CodeForce/Ranked_800/contest_71_problem_A.cpp
@@ -0,0 +1,29 @@
+//
+// Created by Shoul on 1/7/2025.
+//
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+
+int main() {
+    string word;
+
+    int number;
+    cin >> number;
+    for (int i = 0; i < number; i++) {
+        cin >> word;
+        if (word.length() > 10) {
+            cout << word[0] << word.length() - 2 << word[word.length() - 1]<<endl;
+
+        }
+        else {
+            cout << word<<endl;
+        }
+    }
+
+    return 0;
+}
+
+
